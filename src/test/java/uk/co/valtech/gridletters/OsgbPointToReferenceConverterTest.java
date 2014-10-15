26430684d83399d96b2ed9f93d6df6c439e85a69
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2014-10-15 15:39:11 +0100

    Completed work

--- src/test/java/uk/co/valtech/gridletters/OsgbPointToReferenceConverterTest.java
+++ src/test/java/uk/co/valtech/gridletters/OsgbPointToReferenceConverterTest.java
@@ -1,38 +1,38 @@
 package uk.co.valtech.gridletters;
 
-import org.junit.Before;
 import org.junit.Test;
+import uk.co.valtech.gridletters.domain.Reference;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static uk.co.valtech.gridletters.domain.Units.*;
 
-public class OsgbPointToReferenceTest {
-    private OsgbPointToReference instance;
-
-    @Before
-    public void setUp() throws Exception {
-        instance = new OsgbPointToReference();
-    }
+public class OsgbPointToReferenceConverterTest {
 
 
     @Test
     public void originShouldBeTranslatedCorrectly() throws Exception {
+        OsgbPointToReferenceConverter instance =
+                new OsgbPointToReferenceConverter(Reference.BOX_100km);
+
         int eastings = 0;
         int northings = 0;
-        String expectedGrid = "SV0000000000";
+        String expectedGrid = "SV";
 
         assertThat("Grid reference of: "+eastings+", "+northings+" failed",
-                instance.convert(eastings, northings), is(expectedGrid));
+                instance.toGridReference(eastings, northings), is(expectedGrid));
     }
 
     @Test
     public void positivePointFarFromOrigin() throws Exception {
+        OsgbPointToReferenceConverter instance =
+                new OsgbPointToReferenceConverter(Reference.BOX_1m);
+
         int eastings  = 3 * KM_2500 + 5 * KM_100 + 1 * KM_10 + 2 * KM_1 + 3 * M_100 + 4 * M_10 + 5 * M_1;
         int northings = 2 * KM_2500 + 2 * KM_100 + 6 * KM_10 + 7 * KM_1 + 8 * M_100 + 9 * M_10 + 0 * M_1;
         String expectedGrid = "TL1234567890";
 
         assertThat("Grid reference of: "+eastings+", "+northings+" failed",
-                instance.convert(eastings, northings), is(expectedGrid));
+                instance.toGridReference(eastings, northings), is(expectedGrid));
     }
 }
